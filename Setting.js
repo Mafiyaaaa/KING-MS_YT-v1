cd4a1f49ccf98619fc2da943f0fb111a5c470f01
Mafiyaaaa <emalshadilruvanchamikara.3@gmail.com>
2025-03-14 17:34:24 +0530

    Create Setting.js

---
+++ Setting.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+SESSION_ID: process.env.SESSION_ID === undefined ? 'LnBzWTJJ#5y7WtgeqyWY2savgCbF31ahhVxWVM8GV-7jaJ-OqnmA' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94767861594' : process.env.OWNER_NUMBER,  
+PREFIX: process.env.PREFIX || '.' ,     
+}
